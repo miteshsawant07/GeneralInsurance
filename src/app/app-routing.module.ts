1a1f2a8a93af931619c76043f11d036a82f6c026
miteshsawant07 <miteshsawant07.ms@gmail.com>
2021-04-08 22:37:51 +0530

    refer this for login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{HomePageComponent} from './Components/home-page/home-page.component';
+import{RegisterComponent} from './Components/register/register.component';
+import{LoginComponent} from './Components/login/login.component';
+import{AboutUsComponent} from './Components/about-us/about-us.component';
+import{ContactUsComponent} from './Components/contact-us/contact-us.component';
+const routes: Routes = [
+ // {path:'',component:HomePageComponent,pathMatch:'full'},
+  {path:'register',component:RegisterComponent},
+  {path : 'home-page', component:HomePageComponent},
+  {path : 'Login', component:LoginComponent},
+  {path : 'About-us', component:AboutUsComponent},
+  {path: 'Contact', component:ContactUsComponent}
+  
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
